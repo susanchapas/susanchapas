e06831c520c4f0e3862ba597000bc833e6370036
Susan <susan email>
2025-12-09 19:43:50 -0500

    chore: making config changes to fix build on github chages

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // produce a static export (creates `out/` when running `next export`)
+  output: "export",
   reactCompiler: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
