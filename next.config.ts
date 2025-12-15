985d98eb594fbdeb37fc145af62eafb9e7b730d5
Susan <susan email>
2025-12-14 19:56:38 -0500

    fix: MOREEEEE build errors apparently

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Enable static HTML export
+  output: 'export',
   reactCompiler: true,
 };
 
